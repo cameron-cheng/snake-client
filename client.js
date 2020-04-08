595cb09cb2823aa85cec435adbcf26de098cf9c8
vagrant <Dcameroncheng@gmail.com>
2020-04-08 20:45:54 +0000

    updated files

--- client.js
+++ client.js
@@ -6,17 +6,6 @@ const connect = function() {
     host: '50.64.116.162',
     port: 50541
   });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  console.log('Successfully connected to game server!');
-  conn.write('Name: Cam');
-  // setInterval(() => {
-  //   conn.write('Move: up')
-  // }, 500);
 
   return conn;
 }
--- play.js
+++ play.js
@@ -1,4 +1,33 @@
 // play.js
 const { connect } = require('./client');
 console.log('Connecting ...');
-connect();  
+const conn = connect();  
+
+conn.on('connect', function() {
+  console.log('Connected');
+  conn.write('Name: Cam')
+  // setInterval(() => {
+  //   conn.write('Move: up')
+  // }, 50);
+})
+conn.on('data', (data) => {
+  console.log(data);
+});
+
+const handleUserInput = function(data) {
+  if (data === '\x03') {
+    process.exit()
+  }
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+setupInput();
+
